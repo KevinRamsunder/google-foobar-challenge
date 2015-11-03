213461bd4d44b067202c55f685dc62b34f572773
Kevin Ramsunder <kevinramsunder5@gmail.com>
2015-11-02 21:34:57 -0500

    Added hash it out challenge

--- hash_it_out/hash_it_out.py
+++ hash_it_out/hash_it_out.py
@@ -1,3 +1,11 @@
 def answer(digest):
     code = [0 for i in range(len(digest))]
+
+    for i in range(len(digest)):
+        for j in range(256):
+            cipher = (digest[i] ^ code[i - 1]) + j * 256 
+            if cipher % 129 == 0:
+                code[i] = cipher / 129
+                break
+
     return code
